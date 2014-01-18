01e751e57682f6cc55a4809b06f995570622be40
Dmitry Momot <dmitry@dimkof.com>
2014-01-18 22:01:04 +0200

    moved title buttons ( last page, previous page, etc) to the config file

--- config/pagination.php
+++ config/pagination.php
@@ -7,9 +7,34 @@ return array(
 		'current_page'      => array('source' => 'query_string', 'key' => 'page'), // source: "query_string" or "route"
 		'total_items'       => 0,
 		'items_per_page'    => 10,
-		'view'              => 'pagination/basic',
+		'view'              => 'pagination/bootstrap/basic',
 		'auto_hide'         => TRUE,
 		'first_page_in_url' => FALSE,
+
+		/**
+		 * for floating template
+		 */
+		'count_out'	=> 3,
+		'count_in'	=> 5,
+
+		/**
+		 * Append class to block <ul>
+		 * e.g: pagination-lg, pagination-sm, etc
+		 * <ul class="pagination pagination-lg">
+		 * .....
+		 */
+		'append_class' => NULL,
+
+		/**
+		 * if value equal NULL or FALSE, then it will be hidden
+		 * e.g.
+		 * 		'first_title' => 'First page', 	// First page, Previous, 1, 2, ....
+		 * 		'first_title' => FALSE,			// Previous, 1, 2, ....		 *
+		 */
+		'first_title' 		=> 'First',
+		'previous_title'	=> 'Previous',
+		'next_title' 		=> 'Next',
+		'last_title' 		=> 'Last',
 	),
 
 );
