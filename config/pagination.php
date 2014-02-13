7521e9430bebe55d609d7df656311c3d6002a3ab
Dmitry Momot <dmitry@dimkof.com>
2014-02-13 16:02:29 +0200

    added new params

--- config/pagination.php
+++ config/pagination.php
@@ -7,15 +7,15 @@ return array(
 		'current_page'      => array('source' => 'query_string', 'key' => 'page'), // source: "query_string" or "route"
 		'total_items'       => 0,
 		'items_per_page'    => 10,
-		'view'              => 'pagination/bootstrap/basic',
+		'view'              => 'pagination/bootstrap/floating',
 		'auto_hide'         => TRUE,
 		'first_page_in_url' => FALSE,
 
 		/**
 		 * for floating template
 		 */
-		'count_out'	=> 3,
-		'count_in'	=> 5,
+		'count_out'	=> 2,
+		'count_in'	=> 2,
 
 		/**
 		 * Append class to block <ul>
@@ -31,10 +31,14 @@ return array(
 		 * 		'first_title' => 'First page', 	// First page, Previous, 1, 2, ....
 		 * 		'first_title' => FALSE,			// Previous, 1, 2, ....		 *
 		 */
-		'first_title' 		=> 'First',
-		'previous_title'	=> 'Previous',
-		'next_title' 		=> 'Next',
-		'last_title' 		=> 'Last',
+		// 'first_title' 		=> 'First',
+		// 'previous_title'	=> 'Previous',
+		// 'next_title' 		=> 'Next',
+		// 'last_title' 		=> 'Last',
+		'first_title' 		=> FALSE,
+		'previous_title'	=> '&laquo;',
+		'next_title' 		=> '&raquo;',
+		'last_title' 		=> FALSE,
 	),
 
 );
